0eaafa226c9bbb35c936a1bcfac7662af9d93b61
Ercan Er <iosyazilim@gmail.com>
2019-09-29 00:09:41 +0300

    Aux and Layout

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,19 @@
+import { Component } from 'react';
+import Aux from '../../hoc/Aux';
+
+class BurgerBuilder extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <Aux>
+            <div>Burger</div>
+            <div>Build Controls</div>
+        </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
