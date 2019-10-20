b8bc4ce99e47e4e375ceedebc3acf1aa0116ef06
Ercan Er <iosyazilim@gmail.com>
2019-10-20 20:08:25 +0300

    axios

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,10 +1,17 @@
 import axios from 'axios';
 
-const instance = axios.create();
+const instance = axios.create({
+    baseURL: 'https://burgerbuilder-ea082.firebaseio.com/' //You can write your firebase link
+});
 
 instance.interceptors.request.use(resolve => {
     console.log(resolve);
     return resolve;
 });
+
+instance.interceptors.response.use(response => {
+    console.log(response)
+    return response;
+})
 
 export default instance;
